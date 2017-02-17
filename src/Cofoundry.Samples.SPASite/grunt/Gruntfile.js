3c2eff6cabda03763a6eba0b8064264637191b50
Dominic Walker <dom@dominics-mbp.mynet>
2017-02-17 12:43:20 +0000

    Removed the image minification from the grunt process

--- src/Cofoundry.Samples.SPASite/grunt/Gruntfile.js
+++ src/Cofoundry.Samples.SPASite/grunt/Gruntfile.js
@@ -80,7 +80,6 @@ module.exports = function(grunt) {
     
     grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-contrib-compass');
-    grunt.loadNpmTasks('grunt-contrib-imagemin');
     grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-contrib-concat');
     
