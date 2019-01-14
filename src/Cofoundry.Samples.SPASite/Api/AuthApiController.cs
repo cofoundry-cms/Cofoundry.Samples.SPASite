9adfd006ea5593ef1052afd2b1f16d2463e713e9
Joel Mitchell <me@joelmitchell.co.uk>
2019-01-14 21:51:16 +0000

    Migrated the SPA app to use Vue.js and updated the front end to make it a little prettier.

--- src/Cofoundry.Samples.SPASite/Api/AuthApiController.cs
+++ src/Cofoundry.Samples.SPASite/Api/AuthApiController.cs
@@ -6,6 +6,7 @@ using Cofoundry.Web;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Antiforgery;
+using Cofoundry.Samples.SPASite.Domain;
 
 namespace Cofoundry.Samples.SPASite
 {
@@ -15,17 +16,17 @@ namespace Cofoundry.Samples.SPASite
     {
         private readonly IApiResponseHelper _apiResponseHelper;
         private readonly IAntiforgery _antiforgery;
-        private readonly ICommandExecutor _commandExecutor;
+        private readonly IQueryExecutor _queryExecutor;
 
         public AuthApiController(
             IApiResponseHelper apiResponseHelper,
             IAntiforgery antiforgery,
-            ICommandExecutor commandExecutor
+            IQueryExecutor queryExecutor
             )
         {
             _apiResponseHelper = apiResponseHelper;
             _antiforgery = antiforgery;
-            _commandExecutor = commandExecutor;
+            _queryExecutor = queryExecutor;
         }
 
         #region queries
@@ -34,12 +35,19 @@ namespace Cofoundry.Samples.SPASite
         /// Once we have logged in we need to re-fetch the csrf token because
         /// the user identity will have changed and the old token will be invalid
         /// </summary>
-        [HttpGet("csrf-token")]
-        public IActionResult GetCsrfToken()
+        [HttpGet("session")]
+        public async Task<IActionResult> GetAuthSession()
         {
+            var member = await _queryExecutor.ExecuteAsync(new GetCurrentMemberSummaryQuery());
             var token = _antiforgery.GetAndStoreTokens(HttpContext);
-            
-            return _apiResponseHelper.SimpleQueryResponse(this, token.RequestToken);
+
+            var sessionInfo = new
+            {
+                Member = member,
+                AntiForgeryToken = token.RequestToken
+            };
+
+            return _apiResponseHelper.SimpleQueryResponse(this, sessionInfo);
         }
 
         #endregion
@@ -55,6 +63,13 @@ namespace Cofoundry.Samples.SPASite
         [HttpPost("login")]
         public Task<IActionResult> Login([FromBody] LogMemberInCommand command)
         {
+            return _apiResponseHelper.RunCommandAsync(this, command);
+        }
+
+        [HttpPost("sign-out")]
+        public Task<IActionResult> SignOut()
+        {
+            var command = new LogMemberOutCommand();
             return _apiResponseHelper.RunCommandAsync(this, command);
         }
 
